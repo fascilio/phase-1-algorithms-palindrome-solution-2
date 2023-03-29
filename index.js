d70161f522f1e71c7bdad2d42eba9eb79e7cd7d8
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-03-29 17:55:59 +0300

    pointers

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let leftPointer = 0;
+  let rightPointer = word.length - 1;
+
+  while (leftPointer < rightPointer) {
+    if (word[leftPointer] !== word[rightPointer]) {
+      return false;
+    }
+    leftPointer++;
+    rightPointer--;
+  }
+
+  return true;
+
 }
 
 /* 
   Add your pseudocode here
+  function isPalindrome(word):
+    // Initialize pointers
+    leftPointer = 0
+    rightPointer = length(word) - 1
+
+    // Compare characters from both ends
+    while (leftPointer < rightPointer):
+        if (word[leftPointer] != word[rightPointer]):
+            return false
+        leftPointer += 1
+        rightPointer -= 1
+
+    // If we made it here, the word is a palindrome
+    return true
+
 */
 
 /*
   Add written explanation of your solution here
+  It initializes leftPointer and rightPointer to the first and last character of the word respectively.
+  It compares the characters at the current positions of the pointers, and returns false if they are not equal.
+  It moves the pointers towards each other by incrementing leftPointer and decrementing rightPointer.
+  It continues this process until the pointers cross in the middle of the word, or until a mismatch is found.
+  If the loop completes without returning false, the word is a palindrome, so it returns true.
 */
 
 // You can run `node index.js` to view these console logs
@@ -23,3 +56,6 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+console.log("");
+ console.log("=>", isPalindrome("a"));
+ console.log("=>", isPalindrome("ab"));
